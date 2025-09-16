2da19419271e70f3883793464a6b74a9131ae28e
Manish Yadav <manish133188@gmail.com>
2025-09-16 17:08:32 +0530

    Navbar component added

--- myportfolio/src/components/Navbar/Navbar.jsx
+++ myportfolio/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import {Link} from "react-scroll";
 import { IoMdMenu } from "react-icons/io";
-import pic from "../../../public/photo.avif" 
+import pic from "../../../public/photo.avif";
+import { MdOutlineMenu } from "react-icons/md";
+import { AiOutlineClose } from "react-icons/ai";
 
 
 
 const Navbar = () =>{
+
+
+  const [menu, setmenu] = useState(false);
+  const navItems =[
+    {
+      id:1,
+      text:"Home"
+    },
+    {
+      id:2,
+      text: "About"
+    },
+    {
+      id:3,
+      text:"Portfolio"
+    },
+    {
+      id:4,
+      text:"Experience"
+
+    },
+    {
+      id:5,
+      text:"Contacts"
+    }
+  ]
   return(
     <>
-    <div className="max-w-screen-2xl container mx-auto px-4  md:px-20 h-16 shadow-md ">
-      <div className="flex justify-between ">
+    <div className="max-w-screen-2xl container mx-auto px-4  md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 ">
+      <div className="flex justify-between h-16 ">
         <div className="flex space-x-2">
           <img src={pic} className="h-12 w-12 rounded-full" alt="menu icon" />
           <h1 className="font-semibold text-xl cursor-pointer">Mani<span className="text-green-500 text-2xl">sh</span>
@@ -19,15 +47,29 @@ const Navbar = () =>{
           
         </div>
         <div className="py-4">
-          <ul className="flex space-x-8">
-            <li className="">Home</li>
-            <li>About</li>
-            <li>Portfolio</li>
-            <li>Contacts</li>
+          <ul className="hidden md:flex space-x-8">
+           {
+            navItems.map(({id,text})=>(
+              <li className="hover:scale-105 duration-200 cursor-pointer" key={id}>{text}</li>
+            ))
+           }
           </ul>
+
+         <div onClick={()=>setmenu(!menu)} className="md:hidden"> {menu? <MdOutlineMenu size={24} />:<AiOutlineClose size={24} />}</div>
         </div>
       </div>
-      <div></div>
+      {menu && (
+         <div>
+          <ul className="md:hidden flex flex-col items-center justify-center py-4">
+             {
+            navItems.map(({id,text})=>(
+              <li className="hover:scale-105 duration-200 cursor-pointer font-semibold" key={id}>{text}</li>
+            ))
+           }
+          </ul>
+      </div>
+      )}
+     
 
     </div>
     </>
